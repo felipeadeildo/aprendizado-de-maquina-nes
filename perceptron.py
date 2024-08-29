8f51aa583e09b4635253fbea3a40513e22ef5e88
Felipe Adeildo <felipeadeildo@duck.com>
2024-08-29 15:26:23 -0300

    refatora perceptron para iterar até convergir (se convergir)

--- perceptron.py
+++ perceptron.py
@@ -1,5 +1,5 @@
 import random
-from typing import List
+from typing import List, Union
 
 
 class Perceptron:
@@ -18,7 +18,7 @@ class Perceptron:
         n_iters (int): O número de iterações do algoritmo. Valor padrão é 1000.
     """
 
-    weights: List[float]
+    weights: List[Union[float, int]]
 
     def __init__(self, learning_rate: float = 0.01, n_iters: int = 1000):
         """Inicializa o Perceptron com uma taxa de aprendizado e número de iterações.
@@ -29,16 +29,26 @@ class Perceptron:
         """
         self.lr = learning_rate
         self.n_iters = n_iters
+        self._iterations = 0
 
-    def fit(self, X: List[List[float]], y: List[float]):
+    @property
+    def iterations(self) -> int:
+        """Retorna o número total de iterações até a convergência.
+
+        Returns:
+            int: O número de iterações realizadas.
+        """
+        return self._iterations
+
+    def fit(self, X: List[List[Union[float, int]]], y: List[Union[float, int]]):
         """Treina o Perceptron usando os dados de treinamento.
 
         Este método ajusta os pesos do Perceptron para minimizar os erros de classificação.
 
         Args:
-            X (List[List[float]]): A matriz de características de entrada, onde cada elemento
+            X (List[List[Union[float, int]]]): A matriz de características de entrada, onde cada elemento
                 é uma lista que representa as características de uma amostra.
-            y (List[float]): O vetor de rótulos de saída, onde cada elemento é o rótulo
+            y (List[Union[float, int]]): O vetor de rótulos de saída, onde cada elemento é o rótulo
                 associado a uma amostra de entrada.
 
         A fórmula para o ajuste dos pesos é:
@@ -51,44 +61,62 @@ class Perceptron:
             x_ij = valor da característica j na amostra i
         """
         self.weights = [random.random() for _ in range(len(X[0]) + 1)]
+        self._iterations = 0
 
         # self.weights[0] é o bias
         X = [[1] + x for x in X]
 
         for _ in range(self.n_iters):
-            self._update_weights(X, y)
+            self._iterations += 1
+            if not self._update_weights(X, y):
+                break
 
-    def _update_weights(self, X: List[List[float]], y: List[float]):
+    def _update_weights(
+        self, X: List[List[Union[float, int]]], y: List[Union[float, int]]
+    ) -> bool:
         """Atualiza os pesos do Perceptron com base nas amostras de treinamento.
 
         Este método percorre cada amostra de treinamento e ajusta os pesos com base no erro
         de predição.
 
-        Args:
-            X (List[List[float]]): A matriz de características de entrada, com o bias adicionado.
-            y (List[float]): O vetor de rótulos de saída.
-
         A fórmula para o ajuste dos pesos é:
             w_j = w_j + η * (y_i - ŷ) * x_ij
+
+        onde:
+            w_j = peso para a característica j
+            η = taxa de aprendizado (learning_rate)
+            y_i = rótulo verdadeiro da amostra i
+            ŷ = predição do Perceptron para a amostra i
+            x_ij = valor da característica j na amostra i
+
+        Args:
+            X (List[List[Union[float, int]]]): A matriz de características de entrada, com o bias adicionado.
+            y (List[Union[float, int]]): O vetor de rótulos de saída.
+
+
+        Returns:
+            bool: True se houve atualização de pesos (ou seja, há pontos mal classificados),
+                  False se todos os pontos foram corretamente classificados.
+
         """
-        # X_i := amostra de treinamento
-        # y_i := rótulo da amostra de treinamento
-        # w_i := peso da amostra de treinamento
-        # w_n+1 := bias.
+        all_classified_correctly = True
 
         for i in range(len(X)):
             y_hat = self.predict(X[i])
-
             error = y[i] - y_hat
 
-            for j in range(len(X[i])):
-                self.weights[j] += self.lr * error * X[i][j]
+            if error != 0:
+                all_classified_correctly = False
+                for j in range(len(X[i])):
+                    self.weights[j] += self.lr * error * X[i][j]
 
-    def predict(self, X: List[float]) -> float:
+        return not all_classified_correctly
+
+    def predict(self, X: List[Union[float, int]]) -> float:
         """Prediz o rótulo de uma amostra de entrada com base nos pesos atuais.
 
         Args:
-            X (List[float]): Uma lista que representa as características de uma amostra.
+            X (List[Union[float, int]]): Uma lista que representa as características de uma amostra.
 
         Returns:
             float: O rótulo predito para a amostra (1 ou 0).
@@ -102,7 +130,6 @@ class Perceptron:
         A função de ativação é então aplicada a z para determinar o rótulo predito.
         """
         z = sum(X[i] * self.weights[i] for i in range(len(X)))
-
         return self._activation(z)
 
     def _activation(self, z: float) -> float:
